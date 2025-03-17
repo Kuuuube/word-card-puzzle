0fda5b0156322fb234c0e8b8b57720e025505331
kuuuube <hexagonisalie@gmail.com>
2025-03-17 18:24:55 -0400

    Handle moving hidden cards to visible

--- js/game.js
+++ js/game.js
@@ -46,41 +46,57 @@ function get_letter() {
     return POSSIBLE_LETTERS[random_index];
 }
 
-function populate_cards() {
-    let cards_grid = document.querySelector("#cards-grid");
-    for (let i = 0; i < CARD_COUNT; i++) {
-        let quiddler_card = document.createElement("div");
-        quiddler_card.classList.add("quiddler-card");
-
-        let hidden_card = document.createElement("div");
-        hidden_card.classList.add(HIDDEN_CARD_CLASS);
-        let hidden_card_image = document.createElement("img");
-        hidden_card.dataset.index = i;
-        hidden_card.dataset.letter = get_letter();
-        hidden_card_image.draggable = false;
-        hidden_card_image.src = ASSETS_BASE_PATH + "quiddler_card_back.png"
-        hidden_card.appendChild(hidden_card_image)
-        quiddler_card.appendChild(hidden_card);
-
-        let visible_card = document.createElement("div");
-        visible_card.classList.add(VISIBLE_CARD_CLASS);
-        let visible_card_image = document.createElement("img");
-        let visible_card_letter = get_letter();
-        visible_card_image.src = ASSETS_BASE_PATH + visible_card_letter + ".png"
-        visible_card.dataset.index = i;
-        visible_card.dataset.letter = visible_card_letter;
-        visible_card_image.draggable = false;
-        visible_card.appendChild(visible_card_image)
+function make_visible_card(index, letter) {
+    let visible_card = document.createElement("div");
+    visible_card.classList.add(VISIBLE_CARD_CLASS);
+    let visible_card_image = document.createElement("img");
+    visible_card_image.src = ASSETS_BASE_PATH + letter + ".png"
+    visible_card.dataset.index = index;
+    visible_card.dataset.letter = letter;
+    visible_card_image.draggable = false;
+    visible_card.appendChild(visible_card_image)
+    if (index !== -1) {
         visible_card.addEventListener("click", (e) => {
             let selected = e.target.classList.contains(SELECTED_CARD_CLASS);
             if (selected) {
                 let index = selected_cards.findIndex((x) => x.index === Number(e.target.parentElement.dataset["index"]));
                 selected_cards.splice(index, 1);
             } else {
-                selected_cards.push({index: i, letter: e.target.parentElement.dataset["letter"]});
+                selected_cards.push({index: index, letter: e.target.parentElement.dataset["letter"]});
             }
             e.target.classList.toggle(SELECTED_CARD_CLASS);
         });
+    }
+    return visible_card;
+}
+
+function make_hidden_card(index, letter) {
+    let hidden_card = document.createElement("div");
+    hidden_card.classList.add(HIDDEN_CARD_CLASS);
+    let hidden_card_image = document.createElement("img");
+    hidden_card.dataset.index = index;
+    hidden_card.dataset.letter = letter;
+    hidden_card_image.draggable = false;
+    if (index === -1) {
+        hidden_card_image.src = ASSETS_BASE_PATH + "placeholder.png";
+    } else {
+        hidden_card_image.src = ASSETS_BASE_PATH + "quiddler_card_back.png";
+    }
+    hidden_card.appendChild(hidden_card_image)
+    return hidden_card;
+}
+
+function populate_cards() {
+    let cards_grid = document.querySelector("#cards-grid");
+    for (let i = 0; i < CARD_COUNT; i++) {
+        let quiddler_card = document.createElement("div");
+        quiddler_card.classList.add("quiddler-card");
+
+        let hidden_card = make_hidden_card(i, get_letter());
+        quiddler_card.appendChild(hidden_card);
+
+        let visible_card_letter = get_letter();
+        let visible_card = make_visible_card(i, visible_card_letter);
         quiddler_card.appendChild(visible_card);
 
         cards_grid.appendChild(quiddler_card);
@@ -97,15 +113,22 @@ function deselect_all_cards() {
 function delete_cards(indexes) {
     let visible_cards = Array.from(document.querySelectorAll("." + VISIBLE_CARD_CLASS));
     let hidden_cards = Array.from(document.querySelectorAll("." + HIDDEN_CARD_CLASS));
-    console.log(hidden_cards);
     for (const element of visible_cards) {
         let visible_card_index = Number(element.dataset["index"]);
         if (indexes.includes(visible_card_index)) {
+            let parent = element.parentElement;
             element.remove();
             let hidden_card = hidden_cards[hidden_cards.findIndex((x) => Number(x.dataset["index"]) === visible_card_index)];
-            hidden_card.classList.add(VISIBLE_CARD_CLASS);
-            hidden_card.classList.remove(HIDDEN_CARD_CLASS);
-            hidden_card.querySelector("img").src = ASSETS_BASE_PATH + hidden_card.dataset["letter"] + ".png";
+            if (hidden_card && Number(hidden_card.dataset["index"]) !== -1) {
+                let new_hidden_card = make_hidden_card(-1, "placeholder");
+                parent.appendChild(new_hidden_card);
+                hidden_card.remove();
+                let new_visible_card = make_visible_card(Number(hidden_card.dataset["index"]), hidden_card.dataset["letter"]);
+                parent.appendChild(new_visible_card);
+            } else {
+                let new_visible_card = make_visible_card(-1, "none");
+                parent.appendChild(new_visible_card);
+            }
         }
     }
 }
